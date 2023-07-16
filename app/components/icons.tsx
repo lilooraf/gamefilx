35f0b776410952c12fa1a77d565c2a9b5a0696ba
lilooraf <lilooraf35@gmail.com>
2023-07-16 01:05:10 -0400

    add component icons

---
+++ app/components/icons.tsx
@@ -0,0 +1,20 @@
+import {
+  ChevronLeft,
+  ChevronRight,
+  Star,
+  BookMarked,
+  X,
+  Plus,
+  type Icon as LucideIcon,
+} from "lucide-react"
+
+export type Icon = LucideIcon
+
+export const Icons = {
+  chevronLeft: ChevronLeft,
+  chevronRight: ChevronRight,
+  star: Star,
+  app: BookMarked,
+  close: X,
+  plus: Plus,
+};
