fc4cea0ef3a935749770591496a21f8b7ff863ba
lilooraf <lilooraf35@gmail.com>
2023-07-16 01:12:29 -0400

    fix icons component

---
+++ components/icons.tsx
@@ -0,0 +1,20 @@
+import {
+  ChevronLeft,
+  ChevronRight,
+  Star,
+  Gamepad2,
+  X,
+  Plus,
+  LucideIcon,
+} from "lucide-react"
+
+export type Icon = LucideIcon
+
+export const Icons = {
+  chevronLeft: ChevronLeft,
+  chevronRight: ChevronRight,
+  star: Star,
+  app: Gamepad2,
+  close: X,
+  plus: Plus,
+};
