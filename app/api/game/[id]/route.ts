09ff459ac5b665b6e12ddd0c4aeadfaaa3bed3e8
lilooraf <lilooraf35@gmail.com>
2023-07-16 00:58:32 -0400

    api routes

---
+++ app/api/game/[id]/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    const res = await fetch(`${process.env.API_URL}/game/${params.id}`, {
+        headers: {
+            'X-RapidAPI-Key': process.env.API_KEY as string,
+            'X-RapidAPI-Host': process.env.API_HOST as string,
+        }
+    });
+    const product = await res.json()
+
+    return NextResponse.json({ product })
+}
