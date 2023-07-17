165c499e0cfa6c2d49b60b5afde6081652b4f412
lilooraf <lilooraf35@gmail.com>
2023-07-17 16:43:34 -0400

    add icons component

--- components/icons.tsx
+++ components/icons.tsx
@@ -6,6 +6,7 @@ import {
   X,
   Plus,
   LucideIcon,
+  ChevronDown,
 } from "lucide-react"
 
 export type Icon = LucideIcon
@@ -13,6 +14,7 @@ export type Icon = LucideIcon
 export const Icons = {
   chevronLeft: ChevronLeft,
   chevronRight: ChevronRight,
+  chevronDown: ChevronDown,
   star: Star,
   app: Gamepad2,
   close: X,
