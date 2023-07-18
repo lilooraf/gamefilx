d93bcc445aadb1b2cc33905525ed0ddb1f9db168
lilooraf <lilooraf35@gmail.com>
2023-07-18 17:29:18 -0400

    redirect unauthorized

--- app/(auth)/new-user/page.tsx
+++ app/(auth)/new-user/page.tsx
@@ -1,8 +1,13 @@
 import { getCurrentUser } from "@/lib/session";
+import { redirect } from "next/navigation";
 
 export default async function NewUserPage() {
 
   const user = await getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
 
   return (
     <div>
