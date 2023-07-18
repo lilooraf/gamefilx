a4c910739a11b6a2efdde07e44f4fa8ed68cbc22
lilooraf <lilooraf35@gmail.com>
2023-07-18 17:19:41 -0400

    handle add to my list

--- components/game/details/actions.tsx
+++ components/game/details/actions.tsx
@@ -1,14 +1,29 @@
 import { GameDetail} from '@/types';
 import { Icons } from '@/components/icons';
+import { Status } from '@prisma/client';
 
 interface GameActionsProps {
   game?: GameDetail;
 }
 
 const GameActions = ({ game }: GameActionsProps) => {
+
+  const handdleAddToMyList = (status?: Status) => {
+    fetch("/api/game/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        game: game,
+        status: status,
+      }),
+    })
+  };
+
   return (
     <>
-      <button className='flex items-center p-1 justify-center text-sm font-bold text-white bg-white/20 active:bg-white/50 transition-colors rounded-md'>
+      <button onClick={() => {handdleAddToMyList()}} className='flex items-center p-1 justify-center text-sm font-bold text-white bg-white/20 active:bg-white/50 transition-colors rounded-md'>
         <Icons.plus />
       </button>
       <button className='flex items-center p-1 justify-center text-sm font-bold text-white bg-white/20 active:bg-white/50 transition-colors rounded-md'>
