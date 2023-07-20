5c127302c60f8f988288094fbe4c96e1d5771e69
lilooraf <lilooraf35@gmail.com>
2023-07-19 20:08:58 -0400

    simplify nav bar with components, add UserAccountNav

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,9 +1,10 @@
-import Link from 'next/link';
-import { Icons } from '@/components/icons';
-import { cn } from '@/lib/utils';
 import { NavItem } from '@/types';
 import { getCurrentUser } from '@/lib/session';
 import { UserAccountNav } from './user-account-nav';
+import { NavLinks } from './nav-links';
+import Link from 'next/link';
+import { Icons } from '@/components/icons';
+import MobileNav from './mobile-nav';
 
 interface NavProps {
   items: NavItem[];
@@ -13,26 +14,17 @@ export default async function NavBar({ items }: NavProps) {
   const user = await getCurrentUser();
 
   return (
-    <nav className='flex mx-4 my-3 justify-between items-center align-middle'>
-      <ul className='flex gap-5 text-xl font-light'>
-        <li>
+    <>
+      <nav className='px-5 py-4 justify-between sticky top-0 bg-gradient-to-b from-white dark:from-black via-white dark:via-black to-transparent z-40 hidden gap-6 lg:flex'>
+        <div className='flex items-center gap-3 pr-3'>
           <Link href='/'>
-            <Icons.app className='text-indigo-600' />
+            <Icons.app className='w-8 h-8 text-indigo-500' />
           </Link>
-        </li>
-        {items.map((item) => (
-          <li key={item.target}>
-            <Link
-              href={item.target}
-              className={cn('flex items-center rounded-md group')}
-            >
-              {item.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-
-      <UserAccountNav user={user} />
-    </nav>
+          <NavLinks items={items} />
+        </div>
+        <UserAccountNav user={user} />
+      </nav>
+      <MobileNav items={items} user={user} />
+    </>
   );
 }
