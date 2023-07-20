ca83b67a0d86dbdb9f8b148fdc1ade7f295dfba9
lilooraf <lilooraf35@gmail.com>
2023-07-19 20:36:11 -0400

    add db seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,78 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log(`Start seeding ...`);
+
+  const platforms = [
+    {
+      name: 'pc',
+      longName: 'PC',
+    },
+    {
+      name: 'ps5',
+      longName: 'PlayStation 5',
+    },
+    {
+      name: 'ps4',
+      longName: 'PlayStation 4',
+    },
+    {
+      name: 'vita',
+      longName: 'PlayStation Vita',
+    },
+    {
+      name: 'xbsx',
+      longName: 'Xbox Series X',
+    },
+    {
+      name: 'xb1',
+      longName: 'Xbox One',
+    },
+    {
+      name: 'wii-u',
+      longName: 'Wii U',
+    },
+    {
+      name: 'switch',
+      longName: 'Nintendo Switch',
+    },
+    {
+      name: 'oculus',
+      longName: 'Oculus',
+    },
+    {
+      name: 'vive',
+      longName: 'HTC Vive',
+    },
+    {
+      name: 'psvr',
+      longName: 'PlayStation VR',
+    },
+    {
+      name: '3ds',
+      longName: 'Nintendo 3DS',
+    },
+    {
+      name: 'stadia',
+      longName: 'Google Stadia',
+    },
+  ];
+
+  await prisma.platform.createMany({
+    data: platforms,
+  });
+
+  console.log(`Seeding finished.`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
