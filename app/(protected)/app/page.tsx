2942b2fb4905250d2ba19b02bf80af17338c9ff4
lilooraf <lilooraf35@gmail.com>
2023-07-22 17:06:17 -0400

    use SectionsUserPlatforms

--- app/(protected)/app/page.tsx
+++ app/(protected)/app/page.tsx
@@ -1,5 +1,5 @@
 import { GamesSection } from "@/components/game/section"
-import { GamesUserPlatforms } from "@/components/game/games-user-platform"
+import { SectionsUserPlatforms } from "@/components/game/sections-user-platform"
 import { gameLists } from "@/config/game-lists"
 
 const GamesPage = () => {
@@ -9,7 +9,7 @@ const GamesPage = () => {
       {gameLists.map((gameList, index) => (
         <GamesSection key={gameList.title} gameList={gameList} id={index} />
       ))}
-      <GamesUserPlatforms />
+      <SectionsUserPlatforms />
     </div>
   )
 }
