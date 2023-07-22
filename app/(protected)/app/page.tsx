77e46fbca12405cac2a1ae34192cc9dc1c77a51f
lilooraf <lilooraf35@gmail.com>
2023-07-22 17:04:43 -0400

    change to server component

--- app/(protected)/app/page.tsx
+++ app/(protected)/app/page.tsx
@@ -1,83 +1,17 @@
-"use client"
-import { useState } from "react"
-
 import { GamesSection } from "@/components/game/section"
-import { Button } from "@/components/ui/button"
-import { useUser } from "@/hooks/use-user"
-import { PlatformType } from "@/types"
-import { Modal } from "@/components/modal"
-import { UserPlatformsForm } from "@/components/user-platforms-form"
-import { observer } from "@legendapp/state/react"
+import { GamesUserPlatforms } from "@/components/game/games-user-platform"
+import { gameLists } from "@/config/game-lists"
 
-const GamesPage = observer(() => {
-  const user = useUser()
-  const [showModal, setShowModal] = useState(false)
+const GamesPage = () => {
 
   return (
     <div>
-      <GamesSection
-        gameList={{
-          title: "Popular",
-          RequestType: "popular",
-        }}
-        id={1}
-      />
-      <GamesSection
-        gameList={{
-          title: "Hall of Fame",
-          RequestType: "hall-of-fame",
-        }}
-        id={2}
-      />
-      <GamesSection
-        gameList={{
-          title: "Upcoming",
-          RequestType: "upcoming",
-        }}
-        id={3}
-      />
-      {user.platforms.get()?.length ? (
-        <div className="mx-4 border-b-2 border-slate-200/20 p-2 pl-8">
-          <p className="text-2xl font-bold">For your platforms 👾</p>
-        </div>
-      ) : (
-        <div className="flex items-center justify-center pb-2">
-          <Button
-            variant="ghost"
-            className=" font-bold"
-            onClick={() => {
-              setShowModal(true)
-            }}
-          >
-            Add your platforms
-          </Button>
-        </div>
-      )}
-      {user.platforms.get()?.map((platform, index) => (
-        <GamesSection
-          key={platform.name}
-          gameList={{
-            title: platform.longName,
-            RequestType: "platforms",
-            platforms: [platform.name as PlatformType],
-          }}
-          id={index + 4}
-        />
+      {gameLists.map((gameList, index) => (
+        <GamesSection key={gameList.title} gameList={gameList} id={index} />
       ))}
-      <Modal
-        isOpen={showModal}
-        onClose={() => {
-          setShowModal(false)
-        }}
-        className="bg-slate-200 dark:bg-slate-800"
-      >
-        <div className="flex flex-col items-center justify-center gap-8">
-          <p className="text-xl font-bold">Select your platforms</p>
-          <UserPlatformsForm />
-        </div>
-      </Modal>
+      <GamesUserPlatforms />
     </div>
   )
-})
+}
 
 export default GamesPage
